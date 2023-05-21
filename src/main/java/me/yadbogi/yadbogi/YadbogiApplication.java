cc5b5b68340658dae9e1def41cc0ffeec793cb77
Loopy_BAEK <likipop@naver.com>
2023-05-21 18:26:02 +0900

    initial commit

---
+++ src/main/java/me/yadbogi/yadbogi/YadbogiApplication.java
@@ -0,0 +1,13 @@
+package me.yadbogi.yadbogi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YadbogiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YadbogiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/yadbogi/yadbogi/YadbogiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.yadbogi.yadbogi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YadbogiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
